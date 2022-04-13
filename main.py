93440cb9d3eb2bc9e909978aab83a7bf4b9a1a5e
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2022-04-13 12:53:55 -0300

    Arquivo main.py
    Arquivo destinado a executar o programa.

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from plotar_sitio import *
+# Importar o arquivo com as funções principais
 from funcoes_principais import *
 
-nome_arquivo = ''  # path relativo ou path absoluto do arquivo PDB (.pdb)
-cadeia_prot = ''   # cadeia da proteína (ex: A ou B ou C ...)
-hetatm = ''        # sigla da molécula (ou átomo) a ser analisada (ver no arquivo PDB a sigla da molécula (ou átomo))
+# Path relativo ou absoluto do arquivo PDB (.pdb)
+nome_arquivo = ''
+
+# Cadeia da proteína (ex: A ou B ou C ...)
+cadeia_prot = ''
+
+# Sigla da molécula ligante (ou átomo)
+hetatm = ''
+
+# Determinar o limiar de distância entre os átomos dos aminoácidos do sítio de ligação
+# e os átomos da molécula ligante
+limiar = 0
+
+# Determinar o tamanho dos pontos que serão plotatos no gráfico (padrão 50)
+tamanho = 50
+
+# Se "True" é possível ver a molécula na plotagem. Se "False" não é possível de ver a
+# molécula na plotagem (padrão "True")
+ver_molecula = True
+
 
 def main():
     # Ler o documento
@@ -17,13 +34,18 @@ def main():
 
     # Obter os aminoácidos do sítio de ligação e as coordenadas XYZ dos átomos da molécula ligante
     # que estão neste sítio
-    sitio, coord_molecula = sitio_ligacao(molecula, cadeia, 4)
+    sitio, coord_molecula = sitio_ligacao(molecula, cadeia, limiar)
 
     # Obter as coordenadas XYZ dos átomos dos aminoácidos do sítio de ligação
-    coord_aminoacidos = atomos_aminoacidos(cadeia_prot, linhas, sitio)
+    atomos_aa_sitio = atomos_aminoacidos_sitio(cadeia, sitio)
+
+    # Juntar as coordenadas dos átomos dos aminoácidos em um dicionário para que haja somente um
+    # aminoácido como chave e o valor seja todas as coordenadas desse aminoácido, ou seja, se houver
+    # mais de um mesmo aminoácido, colocar essas coordenadas na mesma chave
+    coord_aminoacidos = juntar_atomos_aminoacidos_sitio(atomos_aa_sitio)
 
     # Plotar o gráfico
-    plotar(coord_aminoacidos, coord_molecula)
+    plotar_aminoacidos(coord_aminoacidos, coord_molecula, tamanho, ver_molecula)
 
 
 if __name__ == '__main__':
